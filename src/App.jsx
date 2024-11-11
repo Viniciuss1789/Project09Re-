dab1dca938afb96f526081dafba21f35628e2912
Vinicius <161778225+Viniciuss1789@users.noreply.github.com>
2024-11-11 19:37:59 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Cronometro from "./components/Cronometro";
+
+function App() {
+    return(
+        <>
+        <Cronometro/>
+        </>
+    )
+}
+
+export default App;
